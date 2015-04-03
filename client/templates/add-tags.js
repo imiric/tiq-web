cb637720622beaa8d00d0518b473353e693e7d60
Ivan Mirić <imiric@gmail.com>
2015-04-03 19:03:04 +0200

    Add contenteditable tagging implementation
    There are a whole bunch of small unrelated changes here I can't be
    bothered to isolate, so deal with this messy commit.
    
    Basically this displays the text and tags as a header and a list of
    items beneath it, and sets them as contenteditable for a simple UI.
    Adding new text or tags can be done by typing and pressing Enter or
    Tab (or just clicking outside of the element (maybe I should change
    this?)).

--- client/templates/add-tags.js
+++ client/templates/add-tags.js
@@ -1,8 +1,52 @@
-Template.addTags.events({
-  'click button': function(ev, template) {
-    var text = template.$('input[name="text"]').val(),
-        tags = template.$('input[name="tags"]').val().split(',');
+var addTiq = function(ctx, el, template) {
+  var text = el.text();
 
-    Meteor.call('associateTags', text, tags);
+  if (ctx.text) {
+    // Adding a new tag to existing text
+    Meteor.call('associateTags', ctx.text, [text]);
+    template.$('.tag').eq(0).focus();
+  } else {
+    // Adding new text
+    el.removeClass('placeholder');
+    Meteor.call('associateTags', text, []);
+    Router.go('home', {text: text});
+    template.$('.tag').eq(0).focus();
+  }
+};
+
+Template.addTags.events({
+  'keydown .text, keydown .tag': function(event, template) {
+    if (_.contains([9, 13], event.keyCode)) {
+      event.target.blur();
+      return false;
+    }
+  },
+
+  'blur .text, blur .tag': function(event, template) {
+    var el = template.$(event.target),
+        text = el.text(),
+        elType = el.hasClass('tag') ? 'tag' : 'text';
+
+    if (!text) {
+      el.addClass('placeholder');
+      el.text('add ' + elType);
+    } else if (text != this && text != this.text) {
+      addTiq(this, el, template);
+    }
+
+    return false;
+  },
+
+  'click .tag': function(event, template) {
+    // Stop handling anchor clicks
+    // TODO: Override this with a Shift+click.
+    return false;
+  },
+
+  'focus .text, focus .tag': function(event, template) {
+    var el = template.$(event.target);
+    if (el.hasClass('placeholder')) {
+      el.text('');
+    }
   }
 });
