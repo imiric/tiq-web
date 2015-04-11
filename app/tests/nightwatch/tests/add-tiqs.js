f7d233a394c39176c4ed3eb12e6aa1c4849cd702
Ivan Mirić <imiric@gmail.com>
2015-04-12 01:05:57 +0200

    Add tests for encoded text in URLs

--- app/tests/nightwatch/tests/add-tiqs.js
+++ app/tests/nightwatch/tests/add-tiqs.js
@@ -133,6 +133,46 @@ module.exports = {
         this.elementIdText(result.value[0].ELEMENT, function(result) {
           this.assert.equal(result.value, 'cities', '... which is "cities"');
         });
+      });
+  },
+
+  'new text should be encoded for safe URL usage': function(client) {
+    var text = 'http://google.com/',
+        encodedText = encodeURIComponent(text),
+        expectedUrl = client.globals.baseUrl + '/' + encodedText;
+
+    client
+      .click('h1 a')
+      .click(client.globals.placeholders.text.sel)
+      .keys([text, client.Keys.ENTER])
+      .pause(100)
+      .assert.urlEquals(expectedUrl)
+      .getText('h3.text', function(result) {
+        this.assert.equal(result.value, text, 'header text is unencoded');
+      })
+      .assert.attributeEquals('.latest-tiqs li:first-child > a', 'href', expectedUrl)
+      .click(client.globals.placeholders.tag.sel)
+      .keys(['sites', client.Keys.ENTER]);
+  },
+
+  'new tags should have an encoded href': function(client) {
+    var sites = ['http://amazon.com/', 'http://imiric.com/'];
+
+    client
+      .click('#main .tags a:first-child')
+      .click(client.globals.placeholders.tag.sel)
+      .keys([sites[0], client.Keys.ENTER])
+      .keys([sites[1], client.Keys.ENTER])
+      .pause(100)
+      .elements('css selector', '#main .tags a', function(result) {
+        var val = result.value;
+        this.assert.equal(val.length, 3, 'tag list has 3 elements');
+        this.elementIdAttribute(val[val.length-1].ELEMENT, 'href', function(result) {
+          this.assert.equal(
+            result.value,
+            client.globals.baseUrl + '/' + encodeURIComponent(sites[1]),
+            'tag href is encoded');
+        });
       })
       .end();
   },
